3c39f2a4a38ceb97d0cbfabe444748c90e50edd1
daniel-yang66 <daniel.yang787@outlook.com>
2023-12-08 16:55:50 -0500

    using new api

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-import requests
+from FlightRadar24 import FlightRadar24API
+
 from dash import Dash, html, dcc
 from dash.dependencies import Output, Input
 from dash.exceptions import PreventUpdate
@@ -9,122 +10,74 @@ import pandas as pd
 import plotly.express as px
 from datetime import datetime
 
-app = Dash(__name__, external_stylesheets = [dbc.themes.SLATE])
+app = Dash(__name__, external_stylesheets = [dbc.themes.DARKLY])
 server = app.server
 
-load_figure_template('darkly')
+load_figure_template('slate')
 
 app.layout = dbc.Container([
-    html.H2(id='title', children="Real-Time Scheduled & Active Flight Stats", style = {'text-align':'center','font-weight':'bold','font-size':35, 'font-family':'sans-serif'}),
-        dbc.Row(dcc.Link('Powered by aviationstack',href = 'https://aviationstack.com/', target = '_blank'), style = {'text-align':'center','font-size':20}),
+    html.H2(id='title', children="AeroStat", 
+            style = {'margin-left':460,'font-weight':'bold','font-size':40, 'font-family':'sans-serif'}),
+    
+        dbc.Row(dcc.Link('Powered by aviationstack',href = 'https://aviationstack.com/',
+                         style = {'font-size':16,'margin-bottom':12, 'margin-left':460})),
+    
     dbc.Row([
-        dbc.Col([
+        dbc.Row([
         
-        html.H3('Origin Airport:', style={'margin-bottom':15, 'font-size':25, 'font-family':'sans-serif'}),
-        dcc.Input(id='dep',type='text', placeholder='Departure Airport', style = {'text-align':'center','border-radius': 10})]),
-        dbc.Col([
-        html.H3('Destination Airport:', style={'margin-bottom':15,'font-size':25, 'font-family':'sans-serif'}),
-        dcc.Input(id='arr', type='text', placeholder='Arrival Airport', style = {'text-align':'center','border-radius': 10})])], style = {'text-align':'center'}),
-        html.Br(),
+        html.H3('Enter Airport IATA:', 
+                style={'margin-left':-3,'margin-bottom':15, 'font-size':25, 'font-family':'sans-serif','font-weight':'bold'}),
+            
+        dcc.Input(id='dep',type='text', placeholder='Departure Airport', 
+                  style = {'text-align':'center','border-radius': 10,'width':200,'margin-bottom':17,'margin-left':460})], 
+            style = {'text-align':'center'}),
+        
         dbc.Row(
-        html.Button('View Stats', id='submit', style = {'margin-bottom':10,'width':300, 'margin-left':420,'border-radius':30,'background-color':'green','color':'white'}), style = {'text-align':'center'}),
+        html.Button('View Stats', id='submit', 
+                    style = {'margin-bottom':10,'width':300, 'margin-left':420,'border-radius':30,'background-color':'green','color':'white'}), style = {'text-align':'center'}),
         dbc.Row([
         dbc.Col([
             dcc.Graph(id='pie')
         ]),
         dbc.Col(dbc.Row(
-            dcc.Graph(id='bar')),
+            dcc.Graph(id='pie2')),
         )])
         
     
 ])
+])
 
-@app.callback(Output("submit", "n_clicks"),Output('title','children'),Output('pie','figure'), Output('bar','figure'), Input('dep','value'), Input('arr','value'), Input('submit','n_clicks'))
-def view_stats(dep, arr, clicks):
+@app.callback(Output("submit", "n_clicks"),
+              Output('pie','figure'),
+              Output('pie2','figure'), 
+              Input('dep','value'), 
+              Input('submit','n_clicks'))
+
+def view_stats(dep, clicks):
     if not clicks:
         raise PreventUpdate
 
-    params = {
-      'access_key': '349746e955fc67b11e41ece61dfad998',
-        'dep_iata':dep,
-        'arr_iata':arr,
-        'flight_status':'scheduled'
-    }
-    params2 = {
-      'access_key': '349746e955fc67b11e41ece61dfad998',
-        'dep_iata':dep,
-        'arr_iata':arr,
-        'flight_status':'active'
-    }
-
-    response = requests.get('http://api.aviationstack.com/v1/flights', params)
-    data = response.json()
-    response2 = requests.get('http://api.aviationstack.com/v1/flights', params2)
-    data2 = response2.json()
+    fr_api = FlightRadar24API()
+    airport = fr_api.get_airport_details(dep.upper())
     
-    data_all = data['data'] + data2['data']
-    
-    title = f"Flight Stats | {dep.upper()} - {arr.upper()}"
-    
-    airlines = []
-    delayed = []
-    iata = []
-    unique_flights = []
-    flights = filter(lambda x: x['flight_date'] == datetime.now().strftime('%Y-%m-%d'), data_all)
-    for flight in flights:
-        if flight['flight']['codeshared']!=None:
-            if flight['flight']['codeshared']['flight_number'] not in unique_flights:
-                unique_flights.append(flight['flight']['codeshared']['flight_number'])
-                airline_lst = flight['flight']['codeshared']['airline_name'].split()
-                new_lst = []
-                for word in airline_lst:
-                    new_word = word.title()
-                    new_lst.append(new_word)
-                airline = (' ').join(new_lst)
-                
-                delay = 0
-                if flight['departure']['actual'] != None and flight['departure']['scheduled'] < flight['departure']['actual']:
-                    delay += (datetime.strptime(flight['departure']['actual'][0:10] + ' '+ flight['departure']['actual'][11:19], '%Y-%m-%d %H:%M:%S') - datetime.strptime(flight['departure']['actual'][0:10] + ' '+ flight['departure']['actual'][11:19], '%Y-%m-%d %H:%M:%S')).total_seconds() / 60
+    dep = pd.DataFrame(airport['airport']['pluginData']['details']['stats']['departures']['today']['quantity'], 
+                      index = [0])
+    arr = pd.DataFrame(airport['airport']['pluginData']['details']['stats']['arrivals']['today']['quantity'], 
+                      index = [0])
+    figure = px.pie(dep, 
+                    values=sorted([int(dep['onTime']), int(dep['delayed']), int(dep['canceled'])]), 
+                    names= ['On Time','Delayed','Canceled'], 
+                    hole = 0.7,
+                   title = 'Departure Performance')
 
-                if delay >= 15:
-                    delay_status = 'Delayed'
-                else:
-                    delay_status = 'On Time'
-                    
-                airlines.append(airline)
-                delayed.append(delay_status)
-                iata.append(flight['departure']['iata'])
-        else:
-            if flight['flight']['number'] not in unique_flights:
-                
-                unique_flights.append(flight['flight']['number'])
-                airline = flight['airline']['name']
-                
-                delay = 0
-                if flight['departure']['actual'] != None and flight['departure']['scheduled'] < flight['departure']['actual']:
-                    delay += (datetime.strptime(flight['departure']['actual'][0:10] + ' '+ flight['departure']['actual'][11:19], '%Y-%m-%d %H:%M:%S') - datetime.strptime(flight['departure']['actual'][0:10] + ' '+ flight['departure']['actual'][11:19], '%Y-%m-%d %H:%M:%S')).total_seconds() / 60
 
-                if delay >= 15:
-                    delay_status = 'Delayed'
-                else:
-                    delay_status = 'On Time'
-                airlines.append(airline)
-                delayed.append(delay_status)
-                iata.append(flight['departure']['iata'])
-            
-    
-
-    df = pd.DataFrame(list(zip(airlines, delayed,iata)), columns=['Airline','Delay Status','Count'])
-    figure = px.pie(df.groupby('Airline').count().reset_index(),values='Count',names='Airline', hole = 0.7, title='Airline Market Share')
-    figure2 = px.bar(df.groupby(['Airline','Delay Status']).count().reset_index(),x='Airline',y='Count',
-                     color='Delay Status',
-                     color_discrete_map={
-                         'N/A':'yellow',
-                         'On Time':'green',
-                         'Delayed':'red'
-                     }, title = 'On Time vs Delay')
+    figure2 = px.pie(arr, 
+                    values=sorted([int(arr['onTime']), int(arr['delayed']), int(arr['canceled'])]), 
+                    names= ['On Time','Delayed','Canceled'], 
+                    hole = 0.7,
+                    title = 'Arrival Performance')
     clicks = None
-    return clicks,title, figure, figure2
+    return clicks, figure, figure2
     
 
 if __name__ == '__main__':
